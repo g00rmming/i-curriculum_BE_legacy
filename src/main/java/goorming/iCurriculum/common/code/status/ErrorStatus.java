cdb75cc6e7c16e67564fed60a5e8db9a563a97d5
s00hwanFE <qnfn120@naver.com>
2024-05-25 19:01:11 +0900

    feat : base 코드 생성

---
+++ src/main/java/goorming/iCurriculum/common/code/status/ErrorStatus.java
@@ -0,0 +1,41 @@
+package goorming.iCurriculum.common.code.status;
+
+import goorming.iCurriculum.common.code.BaseErrorCode;
+import goorming.iCurriculum.common.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+@AllArgsConstructor
+public enum ErrorStatus implements BaseErrorCode {
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
+
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "로그인 인증이 필요합니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ErrorReasonDTO getReason() {
+        return ErrorReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(false)
+                .build();
+    }
+
+    @Override
+    public ErrorReasonDTO getReasonHttpStatus() {
+        return ErrorReasonDTO.builder()
+                .httpStatus(httpStatus)
+                .message(message)
+                .code(code)
+                .isSuccess(false)
+                .build();
+    }
+}
