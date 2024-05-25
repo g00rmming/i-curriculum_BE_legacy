eef33ca38fc962024dfd618376b81659f191c2ea
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-05-25 17:06:36 +0900

    initial commit

---
+++ src/main/java/goorming/iCurriculum/ICurriculumApplication.java
@@ -0,0 +1,13 @@
+package goorming.iCurriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICurriculumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ICurriculumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/goorming/iCurriculum/ICurriculumApplicationTests.java
@@ -0,0 +1,13 @@
+package goorming.iCurriculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ICurriculumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
