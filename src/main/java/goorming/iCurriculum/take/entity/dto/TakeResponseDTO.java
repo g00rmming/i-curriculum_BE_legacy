bc4f2f3f67d6436de0c49e3f7f766cc7c7052aca
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-06-02 21:44:32 +0900

    feat : create ResponseDTO about Take

---
+++ src/main/java/goorming/iCurriculum/take/entity/dto/TakeResponseDTO.java
@@ -0,0 +1,95 @@
+package goorming.iCurriculum.take.entity.dto;
+
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+public class TakeResponseDTO {
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class TakenCourseDTO{
+        private Long takeId;
+        private Long courseId;
+        private String courseCode;
+        private String courseName;
+        private String grade;
+        private Integer takenLevel;
+        private Integer credit;
+        private String categoryName;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class TakenCourseListDTO{
+        List<TakenCourseDTO> takenCourseDTOList;
+    }
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UntakenCourseDTO{
+        private Long courseId;
+        private String courseCode;
+        private String courseName;
+        private Integer level;//학년
+        private Integer credit;
+        private String categoryName;
+        private Integer takenNumber;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UntakenCourseListDTO{
+        List<UntakenCourseDTO> untakenCourseDTOList;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class MemberStatsDTO{
+        private Integer totalTakenCredit;
+        private Integer majorTakenCredit;
+        private Double totalGrade;
+        private Double majorGrade;
+        private Double previousTotalGrade;
+        private Double previousMajorGrade;
+        private TakenCategoryDTO majorEssentialDTO;
+        private TakenCategoryDTO majorSelectiveDTO;
+        private TakenCategoryDTO generalEssentialDTO;
+        private TakenCategoryDTO generalSelectiveDTO;
+        private GeneralCoreDTO generalCoreDTO;
+    }
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class TakenCategoryDTO{
+        private Integer takenCredit;
+        private List<UntakenCourseDTO> untakenTop5CourseDTOList;
+    }
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class GeneralCoreDTO{
+        private TakenCategoryDTO takenCategoryDTO;
+        private Integer takeOne;
+        private Integer takeTwo;
+        private Integer takeThree;
+        private Integer takeFour;
+        private Integer takeFive;
+        private Integer takeSix;
+        private Integer takeCreative;
+    }
+}
