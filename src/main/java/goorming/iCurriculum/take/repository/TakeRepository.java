f8e1a0defdc31b3dfc0deba476b2379d27876cc1
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-06-02 21:43:06 +0900

    feat : create CourseException

--- src/main/java/goorming/iCurriculum/take/repository/TakeRepository.java
+++ src/main/java/goorming/iCurriculum/take/repository/TakeRepository.java
@@ -1,5 +1,6 @@
-package goorming.iCurriculum.take;
+package goorming.iCurriculum.take.repository;
 
+import goorming.iCurriculum.take.entity.Take;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TakeRepository extends JpaRepository<Take,Long> {
