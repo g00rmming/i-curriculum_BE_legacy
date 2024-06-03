f856a5895346bf6b175d07b1732de8e565727e3b
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-06-03 21:18:42 +0900

    add : 사용자가 핵심교양 해당영역을 이미 수강했는지 검사하는 로직 추가

--- src/main/java/goorming/iCurriculum/take/service/util/CourseFilter.java
+++ src/main/java/goorming/iCurriculum/take/service/util/CourseFilter.java
@@ -7,10 +7,8 @@ import goorming.iCurriculum.member.Member;
 import java.util.Arrays;
 import java.util.List;
 
-
-
-
 public class CourseFilter {
+    // GENERAL_CORE 카테고리 배열 정의
     private static final Category[] GENERAL_CORE = {
             Category.GENERAL_CORE_ONE,
             Category.GENERAL_CORE_TWO,
@@ -20,15 +18,16 @@ public class CourseFilter {
             Category.GENERAL_CORE_SIX
     };
 
+    // 과목을 필터링하여 해당 카테고리를 반환하는 메서드
     public static Category filterCourse(Member member, Course course,
                                         List<EssentialCourse> departmentEssentialCourseList) {
         if (isMajorEssential(member, course)) {
             return Category.MAJOR_ESSENTIAL;
         }
         if (isMajorSelective(member, course)) {
-            return Category.MAJOR_ESSENTIAL;
+            return Category.MAJOR_SELECTIVE;
         }
-        if (isGeneralEssential(member, course, departmentEssentialCourseList)) {
+        if (isGeneralEssential(course, departmentEssentialCourseList)) {
             return Category.GENERAL_ESSENTIAL;
         }
         if (isGeneralCore(member, course)) {
@@ -37,6 +36,7 @@ public class CourseFilter {
         return Category.GENERAL_SELECTIVE;
     }
 
+    // 전공 필수 과목인지 확인하는 메서드
     private static Boolean isMajorEssential(Member member, Course course) {
         if (!equalDepartment(member, course)) {
             return false;
@@ -44,6 +44,7 @@ public class CourseFilter {
         return course.getCategory().equals(Category.MAJOR_ESSENTIAL);
     }
 
+    // 전공 선택 과목인지 확인하는 메서드
     private static Boolean isMajorSelective(Member member, Course course) {
         if (!equalDepartment(member, course)) {
             return false;
@@ -51,13 +52,17 @@ public class CourseFilter {
         return course.getCategory().equals(Category.MAJOR_SELECTIVE);
     }
 
-    private static Boolean isGeneralEssential(Member member, Course course,
+    // 교양 필수 과목인지 확인하는 메서드
+    private static Boolean isGeneralEssential(Course course,
                                               List<EssentialCourse> departmentEssentialCourseList) {
         return departmentEssentialCourseList.stream()
                 .anyMatch(essentialCourse -> essentialCourse.getCourse().equals(course));
     }
 
+    // 교양 핵심 과목인지 확인하는 메서드
     private static Boolean isGeneralCore(Member member, Course course) {
+        if(notCoreByMember(member,course))
+            return false;
         if (member.isNotSWConvergence()) {
             return Arrays.stream(GENERAL_CORE)
                     .anyMatch(c -> course.getCategory().equals(c));
@@ -77,7 +82,24 @@ public class CourseFilter {
         return course.getCategory().equals(Category.GENERAL_CREATIVE);
     }
 
+    // 멤버의 학과와 과목의 학과가 동일한지 확인하는 메서드
     private static Boolean equalDepartment(Member member, Course course) {
         return member.getDepartment().equals(course.getDepartment());
+    }
+
+    // 특정 멤버에게 핵심 교양인지 확인하는 메서드
+    private static Boolean notCoreByMember(Member member, Course course) {
+        if (member.isNotSWConvergence()) { // 21보다 선배인 경우
+            Integer takenCoreCredit = member.getTakeList().stream()
+                    .filter(take -> Arrays.stream(GENERAL_CORE).toList().contains(take.getCategory()))
+                    .mapToInt(take -> take.getCourse().getCredit())
+                    .sum();
+            return takenCoreCredit >= 9;
+        }
+
+        return !(member.getTakeList().stream()
+                .filter(take -> take.getCategory().equals(course.getCategory()))
+                .toList()
+                .isEmpty());
     }
 }
