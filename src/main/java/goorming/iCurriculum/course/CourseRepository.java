4b675c1684a5e013b2126d011ae4981d5c2b5b21
qnfn120 <101265039+genius00hwan@users.noreply.github.com>
2024-06-03 21:19:18 +0900

    add : 수업 이름과 학수번호로 미수강 과목 검색하는 쿼리 추가

--- src/main/java/goorming/iCurriculum/course/CourseRepository.java
+++ src/main/java/goorming/iCurriculum/course/CourseRepository.java
@@ -5,10 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface CourseRepository extends JpaRepository<Course,Long> {
-    @Query("SELECT c FROM Course c WHERE c.id IN :ids")
-    List<Course> findCoursesByIds(@Param("ids") List<Long> ids);
-    @Query("SELECT c FROM Course c WHERE c.id NOT IN :ids ORDER BY c.takenNumber DESC")
-    List<Course> findCoursesNotInIds(@Param("ids") List<Long> ids);
+    public interface CourseRepository extends JpaRepository<Course,Long> {
+        @Query("SELECT c FROM Course c WHERE c.id IN :ids")
+        List<Course> findCoursesByIds(@Param("ids") List<Long> ids);
+        @Query("SELECT c FROM Course c WHERE c.id NOT IN :ids ORDER BY c.takenNumber DESC")
+        List<Course> findCoursesNotInIds(@Param("ids") List<Long> ids);
+
+        @Query("SELECT c FROM Course c WHERE c.id NOT IN :ids AND c.name LIKE concat('%', :searchword, '%')")
+        List<Course> findCourseNotInIdsByCourseName(@Param("ids") List<Long> ids, @Param("searchword") String searchWord);
+
+        @Query("SELECT c FROM Course c WHERE c.id NOT IN :ids AND c.code LIKE concat('%', :searchword, '%')")
+        List<Course> findCourseNotInIdsByCourseCode(@Param("ids") List<Long> ids, @Param("searchword") String searchWord);
 
 }
