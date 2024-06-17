26e18884b80f34399475f74d255d4ddac428143c
maseungmin <maseungmin@maseungmin-ui-MacBookAir.local>
2024-06-17 18:08:20 +0900

    test : ci/cd test

--- src/main/java/goorming/iCurriculum/IndexController.java
+++ src/main/java/goorming/iCurriculum/IndexController.java
@@ -8,7 +8,7 @@ public class IndexController {
 
     @GetMapping("/")
     public String index() {
-        System.out.println("Hello world!!");
+        System.out.println("Hello world!!!");
         return "Hello world";
     }
 
