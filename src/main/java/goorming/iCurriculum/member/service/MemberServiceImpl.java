c99e7c2d4d17b12f8a762648133c2072f7586633
LEE K.S <kingslee0619@gmail.com>
2024-06-19 02:12:01 +0900

    lowercase로 clientId,password 확인

--- src/main/java/goorming/iCurriculum/member/service/MemberServiceImpl.java
+++ src/main/java/goorming/iCurriculum/member/service/MemberServiceImpl.java
@@ -31,10 +31,8 @@ public class MemberServiceImpl implements MemberService{
         newMember.setDepartment(department);
         //일반 회원으로 가입
         newMember.setRoleType(RoleType.ADMIN);
-        newMember.setCreatedAt(LocalDateTime.now());
-        newMember.setUpdatedAt(LocalDateTime.now());
         //비밀번호 brcypt 암호화하여 등록
-        newMember.setPassword(bCryptPasswordEncoder.encode(request.getPassword()));
+        newMember.setPassword(bCryptPasswordEncoder.encode(request.getPassword().toLowerCase()));
 
         return memberRepository.save(newMember);
     }
