7e4b8e908ea9456a78bf48c0fd7e98fa8ae2d497
LEE K.S <108475935+KSLEE19@users.noreply.github.com>
2024-06-19 06:12:45 +0900

    Update SecurityConfig.java

--- src/main/java/goorming/iCurriculum/login/config/SecurityConfig.java
+++ src/main/java/goorming/iCurriculum/login/config/SecurityConfig.java
@@ -56,8 +56,8 @@ public class SecurityConfig {
                             @Override
                             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                                 CorsConfiguration configuration = new CorsConfiguration();
-                                configuration.setAllowedOrigins(Collections.singletonList("*"));
-                                //configuration.setAllowedOrigins(Collections.singletonList("http://grooming-01-s3.s3-website-ap-southeast-1.amazonaws.com"));
+                 
+                                configuration.setAllowedOrigins(Collections.singletonList("http://grooming-01-s3.s3-website-ap-southeast-1.amazonaws.com"));
                                 configuration.setAllowedMethods(Collections.singletonList("*"));
                                 configuration.setAllowCredentials(true);
                                 configuration.setAllowedHeaders(Collections.singletonList("*"));
